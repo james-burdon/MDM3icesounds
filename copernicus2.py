0f8c3d1f1cea6262c3b37fb4e7cffb01443c9295
James Burdon <eq23636@bristol.ac.uk>
2025-10-10 15:18:37 +0100

    new push

--- copernicus2.py
+++ copernicus2.py
@@ -1,9 +1,13 @@
 import copernicusmarine
 import copernicusmarine as cm
 
+
+
 cm.get(
-    dataset_id="OSISAF-GLO-SEAICE_CONC_TIMESERIES-SH-LA-OBS",
+    dataset_id="cmems_mod_glo_phy_my_0.25deg_P1M-m",   # ✅ monthly reanalysis (full-depth)
     output_directory="./data",
-    filter="*2020[1-5]*",      # February 2020
+    filter="*2020_01*",            # ✅ correct pattern for MDS filenames
     overwrite=True
 )
+
+
